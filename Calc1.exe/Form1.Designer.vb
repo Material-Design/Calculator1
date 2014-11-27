8a05f8671e2905b8b3c421e8d76caccc4eebcc64
Material-Design <md@darkkv.org.uk>
2014-11-27 19:46:42 +0000

    Inital upload
    Simple calculator

---
+++ Calc1.exe/Form1.Designer.vb
@@ -0,0 +1,169 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = true
+        Me.Label1.Location = New System.Drawing.Point(39, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(46, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Value 1:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = true
+        Me.Label2.Location = New System.Drawing.Point(39, 70)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(46, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Value 2:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = true
+        Me.Label3.Location = New System.Drawing.Point(126, 51)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(0, 13)
+        Me.Label3.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(16, 154)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 38)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Add"
+        Me.Button1.UseVisualStyleBackColor = true
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(178, 154)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 38)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Clear"
+        Me.Button2.UseVisualStyleBackColor = true
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(97, 154)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 38)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Sub"
+        Me.Button3.UseVisualStyleBackColor = true
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(84, 118)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = true
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 6
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(84, 30)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 7
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(84, 67)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 8
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = true
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.Label4.Location = New System.Drawing.Point(125, 91)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(22, 24)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "="
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = true
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.Label5.Location = New System.Drawing.Point(14, 9)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(100, 12)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Github Material-Design"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.ClientSize = New System.Drawing.Size(267, 212)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Form1"
+        Me.Text = "calc1.exe"
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+End Class
---
+++ Calc1.exe/Form1.vb
@@ -0,0 +1,55 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'check if a numeric value has been used and that the textbox is not empty
+        If TextBox2.Text = "" Or TextBox3.Text = "" Or Not IsNumeric(TextBox2.Text) Or Not IsNumeric(TextBox3.Text) Then
+            MsgBox("Please enter a valid value into the text box")
+        Else
+            'setting label to display action which has occurred e.g '+', '-' etc 
+            Label3.Text = "+"
+            'defining text box variables as Double to enable use of decimal point when adding
+            Dim num1 As Double
+            Dim num2 As Double
+            Dim ans1 As String
+            num1 = TextBox2.Text
+            num2 = TextBox3.Text
+            ans1 = TextBox1.Text
+            ans1 = CDbl(num1) + CDbl(num2)
+            'printing output in TextBox1 
+            TextBox1.Text = ans1
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        'check if a numeric value has been used and that the textbox is not empty
+        If TextBox2.Text = "" Or TextBox3.Text = "" Or Not IsNumeric(TextBox2.Text) Or Not IsNumeric(TextBox3.Text) Then
+            MsgBox("Please enter a valid value into the text box")
+        Else
+            'setting label to display action which has occurred e.g '+', '-' etc 
+            Label3.Text = "-"
+            'defining text box variables as Double to enable use of decimal point when adding
+            Dim num1 As Double
+            Dim num2 As Double
+            Dim ans1 As String
+            num1 = TextBox2.Text
+            num2 = TextBox3.Text
+            ans1 = TextBox1.Text
+            ans1 = CDbl(num1) - CDbl(num2)
+            'printing output in TextBox1 
+            TextBox1.Text = ans1
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ' check if values are clear already, if they are show a popup else clear them
+        If TextBox2.Text = "" Or TextBox3.Text = "" Or Label3.Text = "" Then
+            MsgBox("Nothing to clear")
+        Else
+            Label3.Text = ""
+            TextBox2.Text = ""
+            TextBox3.Text = ""
+            TextBox1.Text = ""
+            MsgBox("Cleared!")
+        End If
+
+    End Sub
+End Class
